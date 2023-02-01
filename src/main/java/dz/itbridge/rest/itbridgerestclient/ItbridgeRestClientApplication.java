f8bb36cea68ad8bc3c597a8c298843564b3ed15c
Mohamed Dernoun <med.dernoun@gmail.com>
2023-01-31 21:21:57 -0500

    first commit

---
+++ src/main/java/dz/itbridge/rest/itbridgerestclient/ItbridgeRestClientApplication.java
@@ -0,0 +1,13 @@
+package dz.itbridge.rest.itbridgerestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbridgeRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItbridgeRestClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dz/itbridge/rest/itbridgerestclient/ItbridgeRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.itbridge.rest.itbridgerestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbridgeRestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
